d8c386c066427c6286f85702a35653be48720107
pacs <pacs@outlook.pt>
2022-05-30 12:01:27 +0100

    now finished

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,6 +1,7 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -9,8 +10,6 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @RestResource(rel="contains-title",path="containsTitle")
-    public List<Post> findByTitleContaining(String title) {
-
-    }
+    List<Post> findByTitleContaining(String title);
 
 }
